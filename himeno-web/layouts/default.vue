4dda2dfdf0008f8bcc60a16116d30c85a00b3b03
aktio <himenoakito1019@gmail.com>
2023-03-13 12:18:23 +0900

    header作成

---
+++ himeno-web/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <AppHeader />
+    <NuxtPage />
+    <p>footer</p>
+  </div>
+</template>
+<script>
+import AppHeader from "../components/organisms/AppHeader";
+export default {
+  components: {
+    AppHeader,
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
